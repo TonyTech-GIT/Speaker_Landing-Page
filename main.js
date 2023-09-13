8d397a6224db9a1ce309d518063d00be2649a8c5
TonyTech-GIT <toomgbebu@gmail.com>
2023-09-13 09:41:14 +0100

    added little animations

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// AOS.init({
+//     duration: 1000,
+//     offset: 100,
+// });
+
+function initializeAOS() {
+    AOS.init({
+        duration: 1000,
+        offset: 100,
+    });
+}
+
+// Initialize AOS when the page initially loads
+window.onload = function () {
+    initializeAOS();
+};
+
+// Reinitialize AOS when DOM changes (e.g., after a hot reload)
+const observer = new MutationObserver(function (mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            // New content has been added to the DOM; reinitialize AOS
+            initializeAOS();
+        }
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+
 const menuBtn = document.getElementById('menu-btn');
 const menuBody = document.getElementById("menuOverlay")
 
